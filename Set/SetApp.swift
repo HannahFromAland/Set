3fe59392c4f7f0f69f02fcae1dbf327af3b46735
HannahFromAland <171098175@smail.nju.edu.cn>
2024-07-28 12:58:42 -0700

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by HannPC on 2024/7/28.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
